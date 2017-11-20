f295dd5cbd6c999e6aa40c6a0ff9668fe1881679
David Basalla <davidbasalla@gmail.com>
2017-11-20 22:14:41 +0000

    Add skull asset

--- src/index.js
+++ src/index.js
@@ -10,16 +10,28 @@ var createScene = function () {
     var scene = new BABYLON.Scene(engine);
 
     // Add a camera to the scene and attach it to the canvas
-    var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene);
-    camera.attachControl(canvas, true);
+    var camera = new BABYLON.FreeCamera("camera1",
+                                          new BABYLON.Vector3(0, 0, -10),
+                                          scene);
+    camera.setTarget(BABYLON.Vector3.Zero());
+    camera.attachControl(canvas, false);
 
     // Add lights to the scene
     var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
     var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
 
-
     // Add and manipulate meshes in the scene
     var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
+    sphere.position.x = 2;
+
+    var assetsManager = new BABYLON.AssetsManager(scene);
+
+    //same old bollocks with not being able to load stuff locally
+    var meshTask = assetsManager.addMeshTask("skull task", "", "assets/", "skull.babylon");
+    meshTask.onSuccess = function (task) {
+        console.log("LOAD SUCCESS");
+    }
+    assetsManager.load();
 
     return scene;
 };
